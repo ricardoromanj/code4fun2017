af415d6912591df3f8cc868e67010c42e9ad2f93
Victor Fernandez <victor.j.fdez@gmail.com>
2017-10-06 16:32:15 -0600

    json api

--- slides/urls.py
+++ slides/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<Event_name>[0-9]+)/$', views.Event, name='event'),
+    url(r'^events/$', views.events, name='events'),
 ]
 
--- slides/views.py
+++ slides/views.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
+from django.core import serializers
 from django.shortcuts import render
 from django.http import HttpResponse
-import datetime
 from slides.models import Event, Presenter 
+import json
+
 
 def index(request):
     events = Event.objects.all()
     return render(request, 'index.html', {'events': events })
 
 def events(request):
-    return render(request, 'events.html', {'events': events })
+    events = Event.objects.all()
+    data = []
+    for event in events:
+        data = data + [{'title': event.name, 'start': str(event.start), 'end': str(event.end)}]
+    data = json.dumps(data)
+    return HttpResponse(data, content_type='application/json')
