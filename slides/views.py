9a9c06175bf18cd3ec1589b43b114d13ea765a4e
Ricardo Roman <r.rmn92@gmail.com>
2017-10-06 14:38:58 -0600

    Working on rendering carousel

--- slides/views.py
+++ slides/views.py
@@ -3,7 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World Slides")
+    template = loader.get_template('slides/index.html')
+    context = { }
+    return render(request, 'slides/index.html', context)
